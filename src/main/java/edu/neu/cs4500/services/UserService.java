b1740822eafde5103b850f3b3833431cc305ec75
Mark <velino.m@husky.neu.edu>
2019-01-21 17:46:36 -0500

    Created service to return all available users

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,6 +1,7 @@
 package edu.neu.cs4500.services;
 
 import edu.neu.cs4500.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,10 +13,19 @@ import java.util.List;
 @RestController
 public class UserService {
 
+    // Create a hard-coded list of users to return when requested
     static List<User> users = new ArrayList();
     static {
         users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
         users.add(new User(234, "bob", "bob", "Bob", "Marley"));
     }
 
+    /**
+     * Gets a list of all the users for the application.
+     * @return A list of all users.
+     */
+    @GetMapping("/api/user")
+    public List<User> findAllUsers() {
+        return users;
+    }
 }
